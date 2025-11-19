afa053b9d0842a13ef073b69005a99fe03e6ca60
Fehér Dániel <feher.daniel.sandor@diak.szbi-pg.hu>
2025-11-19 08:20:06 +0100

    Create Value_ZeroDivision.py

---
+++ Value_ZeroDivision.py
@@ -0,0 +1,11 @@
+try:
+    osztando = 10
+    oszto = float(input(f"Mennyivel osszam a {osztando}-ot?"))
+
+    print(f"A hányados {osztando/oszto}")
+except ZeroDivisionError as e:
+    print(e)
+    print("ZeroDivisionError: Nullával nem osztunk!")
+except ValueError as e:
+    print(e)
+    print("ValueError: Nem számot adtál meg!")
